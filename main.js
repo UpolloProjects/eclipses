c6a2aa1985156c49c644be543d999e9c3b55921f
UpolloProjects <147247921+UpolloProjects@users.noreply.github.com>
2023-10-07 21:15:21 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
-import * as THREE from 'three';
+// main.js
+
+const THREE = require('three');
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
@@ -15,12 +17,12 @@ scene.add( cube );
 camera.position.z = 5;
 
 function animate() {
-	requestAnimationFrame( animate );
+  requestAnimationFrame( animate );
 
-	cube.rotation.x += 0.01;
-	cube.rotation.y += 0.01;
+  cube.rotation.x += 0.01;
+  cube.rotation.y += 0.01;
 
-	renderer.render( scene, camera );
+  renderer.render( scene, camera );
 }
 
 animate();
